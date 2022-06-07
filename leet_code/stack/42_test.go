b1b16c271b9f06546458f611cd3659527f5f18d4
jrmarcco <jrmarcco@gmail.com>
2022-06-07 16:52:52 +0800

    commit msg

---
+++ leet_code/stack/42_test.go
@@ -0,0 +1,45 @@
+package stack
+
+import "testing"
+
+type rect struct {
+	width  int
+	height int
+}
+
+func trap(heights []int) int {
+	var rects []rect
+
+	ans := 0
+	for _, height := range heights {
+		accumulatedWidth := 0
+		for {
+			if len(rects) == 0 {
+				break
+			}
+			if rects[len(rects)-1].height >= height {
+				break
+			}
+			bottom := rects[len(rects)-1].height
+			accumulatedWidth += rects[len(rects)-1].width
+			rects = rects[:len(rects)-1]
+
+			if len(rects) != 0 {
+				var up int
+				if height <= rects[len(rects)-1].height {
+					up = height
+				} else {
+					up = rects[len(rects)-1].height
+				}
+				ans += accumulatedWidth * (up - bottom)
+			}
+		}
+		rects = append(rects, rect{width: accumulatedWidth + 1, height: height})
+	}
+	return ans
+}
+
+func TestTrap(t *testing.T) {
+	t.Logf("%d", trap([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}))
+	t.Logf("%d", trap([]int{4, 2, 0, 3, 2, 5}))
+}
---
+++ leet_code/stack/84_test.go
@@ -0,0 +1,42 @@
+package stack
+
+import "testing"
+
+type rectangle struct {
+	width  int
+	height int
+}
+
+func largestRectangleArea(heights []int) int {
+	// 末尾补 0 保证栈最后能弹空
+	heights = append(heights, 0)
+
+	var rectangles []rectangle
+	ans := 0
+	for _, height := range heights {
+		accumulatedWidth := 0
+		for {
+			if len(rectangles) == 0 {
+				break
+			}
+			topRectangle := rectangles[len(rectangles)-1]
+			if topRectangle.height < height {
+				break
+			}
+			accumulatedWidth += topRectangle.width
+			area := accumulatedWidth * topRectangle.height
+			if area > ans {
+				ans = area
+			}
+			rectangles = rectangles[:len(rectangles)-1]
+		}
+		rectangles = append(rectangles, rectangle{width: accumulatedWidth + 1, height: height})
+	}
+	return ans
+}
+
+func TestLargest(t *testing.T) {
+	t.Logf("%d", largestRectangleArea([]int{2, 1, 5, 6, 2, 3}))
+	t.Logf("%d", largestRectangleArea([]int{2, 3}))
+	t.Logf("%d", largestRectangleArea([]int{2, 4}))
+}
