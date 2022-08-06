d77c0f583ad49bb2ee3d771712b6c4e20c446b25
jrmarcco <jrmarcco@gmail.com>
2022-08-06 18:52:43 +0800

    commit msg

---
+++ leet_code/linear_ist/skiplist_1206/skiplist_test.go
@@ -0,0 +1,20 @@
+package skiplist_1206
+
+type Skiplist struct {
+}
+
+func Constructor() Skiplist {
+	return Skiplist{}
+}
+
+func (sl *Skiplist) Search(target int) bool {
+	return true
+}
+
+func (sl *Skiplist) Add(num int) {
+
+}
+
+func (sl *Skiplist) Erase(num int) bool {
+	return true
+}
